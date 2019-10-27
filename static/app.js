ad7e176feecdf76357c11598ea201a15b69d4e49
ruhi <rdoshi99@berkeley.edu>
2019-10-26 19:20:27 -0700

    functional flask webapp

--- static/app.js
+++ static/app.js
@@ -1,14 +1,16 @@
-var questionNum = 0;                                // keep count of question, used for IF condition.
-var output = document.getElementById('output');		// store id="output" in output variable
-
-
 function getInput() {
     var input = document.getElementById("input").value;
     document.getElementById("input").value = "";
-    writeResponse(input);
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(input);
+    node.appendChild(textnode);
+    document.getElementById("chat").appendChild(node);
+    reply("You are a lol")
 }
 
-function writeResponse(input) {
-    var ans = 'hi';
-    document.getElementById('output').innerHTML = ans;
+function reply(msg) {
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(msg);
+  node.appendChild(textnode);
+  document.getElementById("chat").appendChild(node);
 }
