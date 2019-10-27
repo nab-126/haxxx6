07b78e452e5f8545cf7980eb52124d664d897d06
ruhi <rdoshi99@berkeley.edu>
2019-10-26 20:21:05 -0700

    in the middle of working on get

--- static/app.js
+++ static/app.js
@@ -14,3 +14,30 @@ function reply(msg) {
   node.appendChild(textnode);
   document.getElementById("chat").appendChild(node);
 }
+
+
+
+
+
+function getBotResponse() {
+  var input = $("#input").val();
+  var inputHtml = '<p class="userText"><span>' + input + "</span></p>";
+  $("#input").val("");
+  $("#ouput").append(inputHtml);
+
+  document
+    .getElementById("input")
+    .scrollIntoView({ block: "start", behavior: "smooth" });
+  $.get("/get", { mssg: input }).done(function(data) {
+    var botHtml = '<p class="botText"><span>' + data + "</span></p>";
+    $("#chatbox").append(botHtml);
+    document
+      .getElementById("userInput")
+      .scrollIntoView({ block: "start", behavior: "smooth" });
+  });
+}
+$("#input").keypress(function(e) {
+  if (e.which == 13) {
+    getBotResponse();
+  }
+});
--- webapp.py
+++ webapp.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template("main.html")
     
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(bot.get_response(userText))
+
     
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host='127.0.0.1')
@@ -20,3 +26,6 @@ def respond():
     my_resp = np.random.choice(outputs, size=1)
 
     return my_resp
+
+
+
